5e92de712e9d4d35379c730a64fae315b723ab09
Farokhiddin Ganiev <45600739+Rekhillab@users.noreply.github.com>
2023-01-24 10:37:36 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,3 +6,12 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+
+//for chatbot
+
+window.replainSettings = { id: '231e1f76-6b2e-4d68-8c94-def6ad56cff9' };
+(function(u){var s=document.createElement('script');s.async=true;s.src=u;
+var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+})('https://widget.replain.cc/dist/client.js');
+
